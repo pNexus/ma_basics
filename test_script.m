8cec31d2d65d0728d117b8195cc5866c997135d3
Pirkka <me@me.com>
2020-04-24 15:20:40 +0300

    second variable calculated

--- test_script.m
+++ test_script.m
@@ -5,6 +5,10 @@ close all
 % generate complex noise vector
 N = 10000;
 a = randn(N,1) + sqrt(-1)*randn(N,1);
-m = mean(a)
-v = var(a)
+m_a = mean(a)
+v_a = var(a)
 
+% second one
+b = randn(N,1) + sqrt(-1)*randn(N,1);
+m_b = mean(b)
+v_b = var(b)
